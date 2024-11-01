fe8e38788e11079efbbf143c31141ab54b96a64b
lineralpha <5386041+lineralpha@users.noreply.github.com>
2024-10-31 23:51:52 -0700

    add a client query

---
+++ client/src/pages/books.tsx
@@ -0,0 +1,31 @@
+// import { gql } from "../__generated__/";
+import { gql, useQuery} from "@apollo/client";
+
+const BOOKS = gql`
+    query getBooks($search: BooksSearchInput) {
+        books(search: $search) {
+            isbn
+            title
+            price
+            author {
+                id
+                name
+                books {
+                    title
+                }
+            }
+        }
+    }
+`;
+
+export const Books = () => {
+    const { loading, error, data } = useQuery(BOOKS);
+    if (loading) {
+        console.log("loading ...");
+    }
+    if (error) {
+        console.log("Error:", error.message);
+    }
+
+    return JSON.stringify(data);
+}
